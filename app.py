1565c3dcc67374a542e18e9405fbeb6c0cb6f65b
Adejola Ogunsan <adejolaogunsan@gmail.com>
2023-07-06 22:20:56 +0000

    Style Changes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import git
 from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_behind_proxy import FlaskBehindProxy
 from forms import RegistrationForm
+from flask_sqlalchemy import SQLAlchemy
 
 
 #creating an instance of the Flask class and assigns it to the variable 'app'
@@ -9,12 +10,35 @@ app = Flask(__name__)                    #passing name as an argument; tells Fla
 proxied = FlaskBehindProxy(app)
 app.config['SECRET_KEY'] = 'e396d8bc80ca4a84e00fd241e0f374b0'
 
+#setting up a database for users
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
+
+with app.app_context():
+  db.create_all()
+
+
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit(): # checks if entries are valid
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home')) # if so - send to home page
+        
+    if form.validate_on_submit(): #already in your code file
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/update_server", methods=['POST'])
@@ -29,14 +53,6 @@ def webhook():
 @app.route("/") 
 #associates a URL with a Python function - access the root URL '/'
 #TODO: change this home directory to locate other pages as well
-
-def hello_world():
-    return "<p>Hello, World!</p>"        
-#function associated with the anove URL#it returns 
-#"Hello, WOrld!" as a p tag when the user accesses the URl
-
-
-@app.route('/home')         #home is similar to index from demo
 def home(): #the home function is defined
   return render_template('home.html', subtitle='Home Page')
 #render_template takes the name of the template, subtitle, and text
